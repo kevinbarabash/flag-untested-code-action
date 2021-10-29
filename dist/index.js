ef622cb91f8da435850562f1fa6fc1612fa9acd6
Kevin Barabash <kevinb@khanacademy.org>
2021-10-29 12:00:23 -0400

    extract most of the code in index.ts into main.ts to make it easier to test

--- dist/index.js
+++ dist/index.js
@@ -67935,20 +67935,16 @@ var __webpack_exports__ = {};
 // ESM COMPAT FLAG
 __nccwpck_require__.r(__webpack_exports__);
 
+// EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
+var core = __nccwpck_require__(2186);
+// EXTERNAL MODULE: ./node_modules/@actions/github/lib/github.js
+var github = __nccwpck_require__(5438);
 // EXTERNAL MODULE: external "fs"
 var external_fs_ = __nccwpck_require__(5747);
 var external_fs_default = /*#__PURE__*/__nccwpck_require__.n(external_fs_);
 // EXTERNAL MODULE: external "path"
 var external_path_ = __nccwpck_require__(5622);
 var external_path_default = /*#__PURE__*/__nccwpck_require__.n(external_path_);
-// EXTERNAL MODULE: external "child_process"
-var external_child_process_ = __nccwpck_require__(3129);
-// EXTERNAL MODULE: external "util"
-var external_util_ = __nccwpck_require__(1669);
-// EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
-var core = __nccwpck_require__(2186);
-// EXTERNAL MODULE: ./node_modules/@actions/github/lib/github.js
-var github = __nccwpck_require__(5438);
 // EXTERNAL MODULE: ./node_modules/chalk/source/index.js
 var source = __nccwpck_require__(8818);
 var source_default = /*#__PURE__*/__nccwpck_require__.n(source);
@@ -68124,6 +68120,108 @@ const makeReport = (title, messages, deltaReport) => {
 };
 /* harmony default export */ const send_report = (makeReport);
 
+;// CONCATENATED MODULE: ./src/utils/get-base-ref.ts
+/**
+ * This is used to determine what the "base" branch for the current work is.
+ *
+ * - If the `GITHUB_BASE_REF` env variable is present, then we're running
+ *   under Github Actions, and we can just use that. If this is being run
+ *   locally, then it's a bit more tricky to determine the "base" for this
+ *   branch.
+ * - If this branch hasn't yet been pushed to github (e.g. the "upstream" is
+ *   something local), then use the upstream.
+ * - Otherwise, go back through the commits until we find a commit that is part
+ *   of another branch, that's either master, develop, or a feature/ branch.
+ *   TODO(jared): Consider using the github pull-request API (if we're online)
+ *   to determine the base branch.
+ */
+const { execSync, spawnSync } = __nccwpck_require__(3129);
+// NOTE: The final `--` tells git that `ref` is a branch name, and *not* a file or
+// directory name. Without it, git gets confused when there's a branch with the
+// same name as a directory.
+const checkRef = (ref) => spawnSync('git', ['rev-parse', ref, '--']).status === 0;
+const validateBaseRef = (baseRef) => {
+    // It's locally accessible!
+    if (checkRef(baseRef)) {
+        return baseRef;
+    }
+    // If it's not locally accessible, then it's probably a remote branch
+    const remote = `refs/remotes/origin/${baseRef}`;
+    if (checkRef(remote)) {
+        return remote;
+    }
+    // Otherwise return null - no valid ref provided
+    return null;
+};
+const getBaseRef = (head = 'HEAD') => {
+    const { GITHUB_BASE_REF } = process.env;
+    if (GITHUB_BASE_REF) {
+        return validateBaseRef(GITHUB_BASE_REF);
+    }
+    else {
+        let upstream = execSync(`git rev-parse --abbrev-ref '${head}@{upstream}'`, {
+            encoding: 'utf8',
+        });
+        upstream = upstream.trim();
+        // if upstream is local and not empty, use that.
+        if (upstream && !upstream.trim().startsWith('origin/')) {
+            return `refs/heads/${upstream}`;
+        }
+        let headRef = execSync(`git rev-parse --abbrev-ref ${head}`, {
+            encoding: 'utf8',
+        });
+        headRef = headRef.trim();
+        for (let i = 1; i < 100; i++) {
+            try {
+                const stdout = execSync(`git branch --contains ${head}~${i} --format='%(refname)'`, { encoding: 'utf8' });
+                let lines = stdout.split('\n').filter(Boolean);
+                lines = lines.filter((line) => line !== `refs/heads/${headRef}`);
+                // Note (Lilli): When running our actions locally, we want to be a little more
+                // aggressive in choosing a baseRef, going back to a shared commit on only `develop`,
+                // `master`, feature or release branches, so that we can cover more commits. In case,
+                // say, I create a bunch of experimental, first-attempt, throw-away branches that
+                // share commits higher in my stack...
+                for (const line of lines) {
+                    if (line === 'refs/heads/develop' ||
+                        line === 'refs/heads/master' ||
+                        line.startsWith('refs/heads/feature/') ||
+                        line.startsWith('refs/heads/release/')) {
+                        return line;
+                    }
+                }
+            }
+            catch (_a) {
+                // Ran out of history, probably
+                return null;
+            }
+        }
+        // We couldn't find it
+        return null;
+    }
+};
+// Multiple action microservices might encounter this, so give them a canned message to print.
+// Logging from inside this lib didn't seem to make it to the GitHub Actions console, so I'll
+// just pass the string back for them to log.
+const cannedGithubErrorMessage = () => {
+    const { GITHUB_BASE_REF } = process.env;
+    return GITHUB_BASE_REF
+        ? `No valid base ref given. Found \`${GITHUB_BASE_REF}\`, but \`${GITHUB_BASE_REF}\` does not ` +
+            `appear to be a valid branch. Perhaps this is coming from a GitHub pull-request that ` +
+            `you reparented, and the old parent no longer exists. This is a bug on GitHub; unless ` +
+            `you push a new commit, the old base ref won't update. You can try solving this by: \n` +
+            `\t1. Merging the new base branch into your pull-request and re-running your checks.\n` +
+            `\t2. Rebasing your pull-request branch onto the new base branch and re-running your checks.\n` +
+            `\t3. Creating and pushing an empty commit (e.g., \`$ git commit --allow-empty -m ` +
+            `'Trigger checks' && git push\`).`
+        : `No valid base ref given. The envar \`GITHUB_BASE_REF\` was null and no other base ref could ` +
+            `be determined.`;
+};
+/* harmony default export */ const get_base_ref = (getBaseRef);
+
+// EXTERNAL MODULE: external "child_process"
+var external_child_process_ = __nccwpck_require__(3129);
+// EXTERNAL MODULE: external "util"
+var external_util_ = __nccwpck_require__(1669);
 // EXTERNAL MODULE: ./node_modules/minimatch/minimatch.js
 var minimatch = __nccwpck_require__(3973);
 var minimatch_default = /*#__PURE__*/__nccwpck_require__.n(minimatch);
@@ -68231,104 +68329,6 @@ const gitChangedFiles = async (base, cwd) => {
         .filter((path) => !isFileIgnored(cwd, path)));
 };
 /* harmony default export */ const git_changed_files = (gitChangedFiles);
-
-;// CONCATENATED MODULE: ./src/utils/get-base-ref.ts
-/**
- * This is used to determine what the "base" branch for the current work is.
- *
- * - If the `GITHUB_BASE_REF` env variable is present, then we're running
- *   under Github Actions, and we can just use that. If this is being run
- *   locally, then it's a bit more tricky to determine the "base" for this
- *   branch.
- * - If this branch hasn't yet been pushed to github (e.g. the "upstream" is
- *   something local), then use the upstream.
- * - Otherwise, go back through the commits until we find a commit that is part
- *   of another branch, that's either master, develop, or a feature/ branch.
- *   TODO(jared): Consider using the github pull-request API (if we're online)
- *   to determine the base branch.
- */
-const { execSync, spawnSync } = __nccwpck_require__(3129);
-// NOTE: The final `--` tells git that `ref` is a branch name, and *not* a file or
-// directory name. Without it, git gets confused when there's a branch with the
-// same name as a directory.
-const checkRef = (ref) => spawnSync('git', ['rev-parse', ref, '--']).status === 0;
-const validateBaseRef = (baseRef) => {
-    // It's locally accessible!
-    if (checkRef(baseRef)) {
-        return baseRef;
-    }
-    // If it's not locally accessible, then it's probably a remote branch
-    const remote = `refs/remotes/origin/${baseRef}`;
-    if (checkRef(remote)) {
-        return remote;
-    }
-    // Otherwise return null - no valid ref provided
-    return null;
-};
-const getBaseRef = (head = 'HEAD') => {
-    const { GITHUB_BASE_REF } = process.env;
-    if (GITHUB_BASE_REF) {
-        return validateBaseRef(GITHUB_BASE_REF);
-    }
-    else {
-        let upstream = execSync(`git rev-parse --abbrev-ref '${head}@{upstream}'`, {
-            encoding: 'utf8',
-        });
-        upstream = upstream.trim();
-        // if upstream is local and not empty, use that.
-        if (upstream && !upstream.trim().startsWith('origin/')) {
-            return `refs/heads/${upstream}`;
-        }
-        let headRef = execSync(`git rev-parse --abbrev-ref ${head}`, {
-            encoding: 'utf8',
-        });
-        headRef = headRef.trim();
-        for (let i = 1; i < 100; i++) {
-            try {
-                const stdout = execSync(`git branch --contains ${head}~${i} --format='%(refname)'`, { encoding: 'utf8' });
-                let lines = stdout.split('\n').filter(Boolean);
-                lines = lines.filter((line) => line !== `refs/heads/${headRef}`);
-                // Note (Lilli): When running our actions locally, we want to be a little more
-                // aggressive in choosing a baseRef, going back to a shared commit on only `develop`,
-                // `master`, feature or release branches, so that we can cover more commits. In case,
-                // say, I create a bunch of experimental, first-attempt, throw-away branches that
-                // share commits higher in my stack...
-                for (const line of lines) {
-                    if (line === 'refs/heads/develop' ||
-                        line === 'refs/heads/master' ||
-                        line.startsWith('refs/heads/feature/') ||
-                        line.startsWith('refs/heads/release/')) {
-                        return line;
-                    }
-                }
-            }
-            catch (_a) {
-                // Ran out of history, probably
-                return null;
-            }
-        }
-        // We couldn't find it
-        return null;
-    }
-};
-// Multiple action microservices might encounter this, so give them a canned message to print.
-// Logging from inside this lib didn't seem to make it to the GitHub Actions console, so I'll
-// just pass the string back for them to log.
-const cannedGithubErrorMessage = () => {
-    const { GITHUB_BASE_REF } = process.env;
-    return GITHUB_BASE_REF
-        ? `No valid base ref given. Found \`${GITHUB_BASE_REF}\`, but \`${GITHUB_BASE_REF}\` does not ` +
-            `appear to be a valid branch. Perhaps this is coming from a GitHub pull-request that ` +
-            `you reparented, and the old parent no longer exists. This is a bug on GitHub; unless ` +
-            `you push a new commit, the old base ref won't update. You can try solving this by: \n` +
-            `\t1. Merging the new base branch into your pull-request and re-running your checks.\n` +
-            `\t2. Rebasing your pull-request branch onto the new base branch and re-running your checks.\n` +
-            `\t3. Creating and pushing an empty commit (e.g., \`$ git commit --allow-empty -m ` +
-            `'Trigger checks' && git push\`).`
-        : `No valid base ref given. The envar \`GITHUB_BASE_REF\` was null and no other base ref could ` +
-            `be determined.`;
-};
-/* harmony default export */ const get_base_ref = (getBaseRef);
 
 ;// CONCATENATED MODULE: ./src/coverage-report.ts
 const getUncoveredLines = (report) => {
@@ -68454,17 +68454,7 @@ const compareReports = (baseReport, headReport) => {
     return report;
 };
 
-;// CONCATENATED MODULE: ./src/index.ts
-/**
- * This action runs `jest` and reports any type errors it encounters.
- *
- * It expects the path to the `jest` binary to be provided as the first
- * argument, and it runs `jest` in the current working directory.
- *
- * It uses `send-report.js` to support both running locally (reporting to
- * stdout) and under Github Actions (adding annotations to files in the GitHub
- * UI).
- */
+;// CONCATENATED MODULE: ./src/main.ts
 
 
 
@@ -68473,11 +68463,8 @@ const compareReports = (baseReport, headReport) => {
 
 
 
-
-
-
-const src_execProm = (0,external_util_.promisify)(external_child_process_.exec);
-const runJest = (jestBin, jestOpts, spawnOpts) => {
+const main_execProm = (0,external_util_.promisify)(external_child_process_.exec);
+const runJest = (jestBin, jestOpts, spawnOpts, core) => {
     return new Promise((resolve, reject) => {
         core.info(`running ${jestBin} ${jestOpts.join(' ')}`);
         const jest = (0,external_child_process_.spawn)(jestBin, jestOpts, spawnOpts);
@@ -68502,19 +68489,14 @@ const LINE_MODIFIED = 'This line was modified but is untested.';
 const LINES_MODIFIED = 'These lines were modified but are untested.';
 const UNCHANGED_LINE_BECAME_UNTESTED = 'This unchanged line is no longer being tested.';
 const UNCHANGED_LINES_BECAME_UNTESTED = 'These unchanged lines are no longer being tested.';
-async function run() {
-    const jestBin = core.getInput('jest-bin');
-    const workingDirectory = core.getInput('custom-working-directory') || '.';
+const main = async (jestBin, workingDirectory, annotationLevel, baseRef, core) => {
     if (!jestBin) {
         core.info(`You need to have jest installed, and pass in the the jest binary via the variable 'jest-bin'.`);
         process.exit(1);
-        return;
     }
-    const baseRef = get_base_ref();
     if (!baseRef) {
         core.info(`No base ref given`);
         process.exit(1);
-        return;
     }
     const current = external_path_default().resolve(workingDirectory);
     const files = await git_changed_files(baseRef, workingDirectory);
@@ -68522,7 +68504,7 @@ async function run() {
     const jsFiles = files.filter((file) => validExt.includes(external_path_default().extname(file)));
     if (!jsFiles.length) {
         core.info('No JavaScript files changed');
-        return;
+        process.exit(1);
     }
     core.info('changed files: \n' + jsFiles.join('\n'));
     const nonImplRegex = /(_test|\.test|\.fixture|\.stories)\.jsx?$/;
@@ -68567,7 +68549,7 @@ async function run() {
     const jestOpts = ['--coverage', ...jsTestFiles];
     try {
         await core.group('Running jest on HEAD', async () => {
-            await runJest(jestBin, jestOpts, { cwd: workingDirectory });
+            await runJest(jestBin, jestOpts, { cwd: workingDirectory }, core);
         });
     }
     catch (err) {
@@ -68579,10 +68561,10 @@ async function run() {
     core.info('Parsing json output from jest');
     const reportPath = external_path_default().join(current, 'coverage/coverage-final.json');
     const headReport = JSON.parse(external_fs_default().readFileSync(reportPath, 'utf-8'));
-    await src_execProm(`git checkout ${baseRef}`);
+    await main_execProm(`git checkout ${baseRef}`);
     try {
         await core.group(`Running jest on ${baseRef}`, async () => {
-            await runJest(jestBin, jestOpts, { cwd: workingDirectory });
+            await runJest(jestBin, jestOpts, { cwd: workingDirectory }, core);
         });
     }
     catch (err) {
@@ -68598,7 +68580,6 @@ async function run() {
     const uncoveredHeadLines = getUncoveredLines(headReport);
     const uncoveredBaseLines = getUncoveredLines(baseReport);
     const messages = [];
-    const annotationLevel = (core.getInput('annotation-level') || 'warning');
     console.log('determing added/changed lines in implementation files');
     core.info('jsImplFiles: ' + jsImplFiles.join(', '));
     for (const filename of jsImplFiles) {
@@ -68690,7 +68671,6 @@ async function run() {
             }
         });
     }
-    await send_report(`Flag Untested Code`, messages, deltaReport);
     const summaryLines = [
         `## Coverage deltas`,
         `|file|% change|lines covered|lines uncovered|`,
@@ -68706,6 +68686,31 @@ async function run() {
             summaryLines.push(`|${relFile}|${(percent * 100).toFixed(2)}|${covered}|${uncovered}|`);
         }
     }
+    return { deltaReport, messages, summaryLines };
+};
+
+;// CONCATENATED MODULE: ./src/index.ts
+/**
+ * This action runs `jest` and reports any type errors it encounters.
+ *
+ * It expects the path to the `jest` binary to be provided as the first
+ * argument, and it runs `jest` in the current working directory.
+ *
+ * It uses `send-report.js` to support both running locally (reporting to
+ * stdout) and under Github Actions (adding annotations to files in the GitHub
+ * UI).
+ */
+
+
+
+
+async function run() {
+    const jestBin = core.getInput('jest-bin');
+    const workingDirectory = core.getInput('custom-working-directory') || '.';
+    const annotationLevel = (core.getInput('annotation-level') || 'warning');
+    const baseRef = get_base_ref();
+    const { deltaReport, messages, summaryLines } = await main(jestBin, workingDirectory, annotationLevel, baseRef, core);
+    await send_report(`Flag Untested Code`, messages, deltaReport);
     core.setOutput('report', summaryLines.join('\n'));
 }
 run().catch((err) => {
